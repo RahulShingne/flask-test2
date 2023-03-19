70f4c28905f32623fe9f92a53481564cbd234156
RahulShingne <91883427+RahulShingne@users.noreply.github.com>
2023-03-20 01:11:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,35 @@ def predict():
 
     }
     return jsonify(str(result))
+@app.route('/data')
+def get_data():
+    dataset = fetch_latest_dataset()
+    if dataset is None:
+        return 'Failed to fetch dataset.'
+    return dataset.to_html()
+
+def fetch_latest_dataset():
+    owner = "RahulShingne"
+    repo = "flask-test2"
+    branch = "main"
+
+
+    response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/git/trees/{branch}?recursive=1")
+    response.raise_for_status()
+
+    latest_file = None
+    for file in response.json()["tree"]:
+        if file["path"].endswith(".csv") and (latest_file is None or file["sha"] < latest_file["sha"]):
+            latest_file = file
+
+    if latest_file is None:
+        raise Exception("No CSV file found in the repository")
+
+    response = requests.get(f"https://raw.githubusercontent.com/{owner}/{repo}/{branch}/{latest_file['path']}")
+    response.raise_for_status()
+
+    df = pd.read_csv(StringIO(response.text))
+    return df
 
 
 if __name__ == '__main__':
